a5b04843df5c757fd505ade8f01c33e709318947
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-05-31 18:12:26 +0300

    Update locators.py

--- locators.py
+++ locators.py
@@ -1,35 +1,36 @@
 from selenium.webdriver.common.by import By
 
 class MainPageLocators:
-    LOGIN_ACCOUNT_BUTTON = (By.XPATH, ".//button[text()='Войти в аккаунт']") 
-    # Железный поиск по атрибуту ссылки — JS-клик по нему бьет без промахов
+    # Ищем элементы по тегам, классам и ссылкам (href), без русского текста
+    LOGIN_ACCOUNT_BUTTON = (By.XPATH, ".//button[contains(@class, 'button_button')]") 
     PERSONAL_CABINET_BUTTON = (By.XPATH, ".//a[@href='/account']") 
-    CONSTRUCTOR_BUTTON = (By.XPATH, ".//*[text()='Конструктор']") 
+    CONSTRUCTOR_BUTTON = (By.XPATH, ".//a[@href='/']//p[contains(text(),'')] | .//p[contains(@class, 'header__link')]") 
     LOGO_BUTTON = (By.XPATH, ".//header//a[@href='/']") 
     
-    # Разделы конструктора
-    BUNS_TAB = (By.XPATH, ".//span[text()='Булки']/..") 
-    SAUCES_TAB = (By.XPATH, ".//span[text()='Соусы']/..") 
-    FILLINGS_TAB = (By.XPATH, ".//span[text()='Начинки']/..") 
+    # Разделы конструктора (ищем по порядку вкладок)
+    BUNS_TAB = (By.XPATH, "(//div[contains(@class, 'tab_tab')])[1]") 
+    SAUCES_TAB = (By.XPATH, "(//div[contains(@class, 'tab_tab')])[2]") 
+    FILLINGS_TAB = (By.XPATH, "(//div[contains(@class, 'tab_tab')])[3]") 
     ACTIVE_TAB = (By.XPATH, ".//div[contains(@class, 'tab_tab_type_current')]") 
 
 class RegisterPageLocators:
-    NAME_INPUT = (By.XPATH, ".//label[text()='Имя']/../input") 
-    EMAIL_INPUT = (By.XPATH, ".//label[text()='Email']/../input") 
+    # Поля ввода по их типам и порядку на форме
+    NAME_INPUT = (By.XPATH, "(//fieldset//input)[1]") 
+    EMAIL_INPUT = (By.XPATH, "(//fieldset//input)[2]") 
     PASSWORD_INPUT = (By.XPATH, ".//input[@type='password']") 
-    REGISTER_BUTTON = (By.XPATH, ".//button[text()='Зарегистрироваться']") 
-    LOGIN_LINK = (By.XPATH, ".//a[@href='/login']") 
+    REGISTER_BUTTON = (By.XPATH, ".//form//button") 
+    LOGIN_LINK = (By.刻PATH, ".//a[@href='/login']") 
     PASSWORD_ERROR = (By.XPATH, ".//p[contains(@class, 'input__error')]") 
 
 class LoginPageLocators:
-    EMAIL_INPUT = (By.XPATH, ".//label[text()='Email']/../input") 
+    EMAIL_INPUT = (By.XPATH, "(//fieldset//input)[1]") 
     PASSWORD_INPUT = (By.XPATH, ".//input[@type='password']") 
-    ENTER_BUTTON = (By.XPATH, ".//button[text()='Войти']") 
-    SUCCESS_LOGIN_MARK = (By.XPATH, ".//button[text()='Оформить заказ']") 
+    ENTER_BUTTON = (By.XPATH, ".//form//button") 
+    SUCCESS_LOGIN_MARK = (By.XPATH, ".//button[contains(@class, 'button_button_size_large')]") 
 
 class ForgotPasswordPageLocators:
     LOGIN_LINK = (By.XPATH, ".//a[@href='/login']") 
 
 class ProfilePageLocators:
-    # Ищем строго слово 'Выход', как на вашем скриншоте страницы профиля
-    EXIT_BUTTON = (By.XPATH, ".//*[text()='Выход']") 
+    # Кнопка выхода в профиле имеет тип button и идет последней в меню
+    EXIT_BUTTON = (By.XPATH, ".//button[contains(@class, 'Account_button')]") 
