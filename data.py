b7cb2fbfcc008f4aaa3c8d06abdc3dc66aefa789
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-06-05 21:52:38 +0300

    Delete data.py

--- data.py
+++
@@ -1,9 +0,0 @@
-class Urls:
-    BASE_URL = "https://education-services.ru"
-    LOGIN_URL = "https://education-services.rulogin"
-    REGISTER_URL = "https://education-services.ruregister"
-    FORGOT_PASSWORD_URL = "https://education-services.ruforgot-password"
-
-class TestData:
-    EMAIL = "alexandr_test_burgers@yandex.ru"
-    PASSWORD = "password123"
