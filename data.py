507d126cbe97d55e878929647d1674825ea8ac8a
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-06-05 21:15:36 +0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,9 @@
+class Urls:
+    BASE_URL = "https://education-services.ru"
+    LOGIN_URL = "https://education-services.rulogin"
+    REGISTER_URL = "https://education-services.ruregister"
+    FORGOT_PASSWORD_URL = "https://education-services.ruforgot-password"
+
+class TestData:
+    EMAIL = "alexandr_test_burgers@yandex.ru"
+    PASSWORD = "password123"
