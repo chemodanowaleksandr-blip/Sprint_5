58369a96e1fa84dd86a07d4f0a7beb5d01b45230
chemodanowaleksandr-blip <chemodanow.aleksandr@gmail.com>
2026-06-05 12:29:02 +0300

    Update test_constructor.py

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -2,25 +2,22 @@ import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import MainPageLocators, LoginPageLocators, ProfilePageLocators
+from helpers import TestDat
 
 # Актуальный домен стенда
-base_host = "stellarburgers.education-services.ru"
-
+        
 class TestConstructorAndProfile:
     # Зарегистрированный на стенде постоянный аккаунт
-    EMAIL = "alexandr_test_burgers@yandex.ru"
-    PASSWORD = "password123"
-
+    
     def login_helper(self, driver):
         """Вспомогательный метод для авторизации перед тестами профиля"""
-        driver.get(f"https://{base_host}/login")
+        driver.get(f"https://{TestData.BASE_HOST}/login")
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located(LoginPageLocators.EMAIL_INPUT))
-        driver.find_element(*LoginPageLocators.EMAIL_INPUT).send_keys(self.EMAIL)
-        driver.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(self.PASSWORD)
+        driver.find_element(*LoginPageLocators.EMAIL_INPUT).send_keys(TestData.EMAIL)
+        driver.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(TestData.PASSWORD)
         driver.find_element(*LoginPageLocators.ENTER_BUTTON).click()
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located(LoginPageLocators.SUCCESS_LOGIN_MARK))
 
-    def test_go_to_personal_cabinet(self, driver):
         """1. Проверка перехода в личный кабинет"""
         self.login_helper(driver)
         
@@ -74,22 +71,37 @@ class TestConstructorAndProfile:
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located(LoginPageLocators.ENTER_BUTTON))
         assert "login" in driver.current_url
 
-    def test_constructor_tabs_navigation(self, driver):
-        """5. Проверка переходов по вкладкам Конструктора (Булки, Соусы, Начинки)"""
-        driver.get(f"https://{base_host}/")
+        def test_constructor_tabs_sauces(self, driver):
+        """5.1 Проверка перехода на вкладку 'Соусы'"""
+        driver.get(f"https://{TestData.BASE_HOST}/")
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located(MainPageLocators.LOGIN_ACCOUNT_BUTTON))
         
         driver.find_element(*MainPageLocators.SAUCES_TAB).click()
-        WebDriverWait(driver, 5).until(
+        assert WebDriverWait(driver, 5).until(
             EC.text_to_be_present_in_element_attribute(MainPageLocators.SAUCES_TAB, "class", "tab_tab_type_current")
         )
+
+    def test_constructor_tabs_fillings(self, driver):
+        """5.2 Проверка перехода на вкладку 'Начинки'"""
+        driver.get(f"https://{TestData.BASE_HOST}/")
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(MainPageLocators.LOGIN_ACCOUNT_BUTTON))
         
         driver.find_element(*MainPageLocators.FILLINGS_TAB).click()
-        WebDriverWait(driver, 5).until(
+        assert WebDriverWait(driver, 5).until(
             EC.text_to_be_present_in_element_attribute(MainPageLocators.FILLINGS_TAB, "class", "tab_tab_type_current")
         )
 
+    def test_constructor_tabs_buns(self, driver):
+        """5.3 Проверка перехода на вкладку 'Булки'"""
+        driver.get(f"https://{TestData.BASE_HOST}/")
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(MainPageLocators.LOGIN_ACCOUNT_BUTTON))
+        
+        # Сначала кликаем на соусы, чтобы сбросить фокус с булок, так как булки активны по умолчанию
+        driver.find_element(*MainPageLocators.SAUCES_TAB).click()
+        
         driver.find_element(*MainPageLocators.BUNS_TAB).click()
-        WebDriverWait(driver, 5).until(
+        assert WebDriverWait(driver, 5).until(
             EC.text_to_be_present_in_element_attribute(MainPageLocators.BUNS_TAB, "class", "tab_tab_type_current")
         )
+
+        
